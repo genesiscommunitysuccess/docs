cb9b58dd4e1f6e5f566b7a3afaa93743060e9b4d
IA-Walsh <108813134+IA-Walsh@users.noreply.github.com>
2022-11-16 14:27:50 +0000

    PTL-387: Added cloudfront access and imports

--- iac/lib/docs-stack.ts
+++ iac/lib/docs-stack.ts
@@ -1,14 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import { CfnOutput, Duration, RemovalPolicy, Stack } from 'aws-cdk-lib';
 
-const BUCKET_ID = 'test-bucket-1';
 
+/**
+ * This is our PoC site infrastructure, which deploys the site content to a PoC S3 bucket.
+ *
+ * The site redirects from HTTP to HTTPS, using a CloudFront distribution,
+ * Route53 alias record, and ACM certificate.
+ */
 export class DocsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, BUCKET_ID, {
-      versioned: true
+    const BUCKET_ID = 'test-bucket-1';
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, 'cloudfront-OAI', {
+      comment: `OAI for ${name}`
     });
+
+    new CfnOutput(this, 'Site', { value: 'https://docs.genesis.global'});
+
+    const docsTestBucket = new s3.Bucket(this, BUCKET_ID, {
+      versioned: true,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    //Cloudfront access
+    docsTestBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [docsTestBucket.arnForObjects('*')],
+      principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
+
   }
 }
