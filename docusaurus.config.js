3a7d2b4d90c70503d2793bf90a214b1801ef3c09
stavino <89891417+stavino@users.noreply.github.com>
2023-01-08 20:40:37 -0500

    added esbuild config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -174,21 +174,14 @@ module.exports = {
       },
     },
   },
-  webpack: {
+    webpack: {
     jsLoader: (isServer) => ({
-      loader: require.resolve('swc-loader'),
-      options: {
-        jsc: {
-          parser: {
-            syntax: 'typescript',
-            tsx: true,
-          },
-          target: 'es2017',
-        },
-        module: {
-          type: isServer ? 'commonjs' : 'es6',
-        },
-      },
+      loader: require.resolve('esbuild-loader'),
+    options: {
+      loader: 'tsx',
+      format: isServer ? 'cjs' : undefined,
+      target: isServer ? 'node12' : 'es2017',
+    },
     }),
   },
 };
