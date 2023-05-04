c4bfeeece8335f9536b08b53dc7c6208e6c1caec
dbarros1979 <daniel.barros@genesis.global>
2023-05-04 20:26:41 +0000

    changing comments

--- infra/lib/amplify-docs-stack.ts
+++ infra/lib/amplify-docs-stack.ts
@@ -19,7 +19,7 @@ export class AmplifyDocsStack extends cdk.Stack {
       sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
         owner: 'genesiscommunitysuccess',
         repository: 'docs',
-        // We got to use a Personal Access Token to authenticate with GitHub. Currently
+        // We use a Personal Access Token to authenticate with GitHub. Currently
         // the token used here is generated from Nick Payne's (@makeusabrew on GitHub) account
         // The account needs admin privileges on the repository to set up a one-time read-only
         // deploy key (to clone the repo) and webhook (for PR previews). Due to this association
