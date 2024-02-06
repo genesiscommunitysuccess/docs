6e7a4db1ec86c257a231b965784f2f159f9d0315
Matt Walker <matt.walker@genesis.global>
2024-02-06 15:50:23 +0000

    chore: api docs with all frontmatter added FUI-1799

--- plugins/api-docs/processedMap.js
+++ plugins/api-docs/processedMap.js
@@ -17,5 +17,5 @@ module.exports = {
 	"@genesislcap/foundation-login": "14.137.0",
 	"@genesislcap/foundation-reporting": "14.137.0",
 	"@genesislcap/foundation-testing": "14.137.0",
-	"@genesislcap/foundation-zero": "14.137.0",
+	// "@genesislcap/foundation-zero": "14.137.0",
 };
