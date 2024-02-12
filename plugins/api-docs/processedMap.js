1131c3c3c01c8d59b78cc6aa2ae7b3ef10f6f017
Matt Walker <matt.walker@genesis.global>
2024-02-12 10:20:30 +0000

    chore: bump fui versions to latest FUI-1799

--- plugins/api-docs/processedMap.js
+++ plugins/api-docs/processedMap.js
@@ -8,14 +8,13 @@ module.exports = {
 	 * from reprocessing during re-builds and retain my changes as final. You can also toggle "enabled": true/false in
 	 * the manifest.json files itself. Removing this static package ref will always re-process enabled packages.
 	 */
-	"@genesislcap/foundation-comms": "14.137.0",
-	"@genesislcap/foundation-entity-management": "14.137.0",
-	"@genesislcap/foundation-filters": "14.137.0",
-	"@genesislcap/foundation-forms": "14.137.0",
-	"@genesislcap/foundation-header": "14.137.0",
-	"@genesislcap/foundation-layout": "14.137.0",
-	"@genesislcap/foundation-login": "14.137.0",
-	"@genesislcap/foundation-reporting": "14.137.0",
-	"@genesislcap/foundation-testing": "14.137.0",
-	// "@genesislcap/foundation-zero": "14.137.0",
+	"@genesislcap/foundation-comms": "14.148.0",
+	"@genesislcap/foundation-entity-management": "14.148.0",
+	"@genesislcap/foundation-filters": "14.148.0",
+	"@genesislcap/foundation-forms": "14.148.0",
+	"@genesislcap/foundation-header": "14.148.0",
+	"@genesislcap/foundation-layout": "14.148.0",
+	"@genesislcap/foundation-login": "14.148.0",
+	"@genesislcap/foundation-reporting": "14.148.0",
+	"@genesislcap/foundation-testing": "14.148.0",
 };
