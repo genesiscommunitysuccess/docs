325c08f60635f866921b359b2425be27782acbdf
Nick Petalas <nick.petalas@genesis.global>
2024-03-13 12:41:43 +0000

    adjusting dependencies

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,3 +1,9 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
 require("dotenv").config();
 
 const baseUrl = process.env.BASE_URL || "/";
