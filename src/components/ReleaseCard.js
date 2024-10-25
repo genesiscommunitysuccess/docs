7de2008093bdbabe448e2ae6be4fd29964c9436d
Artur Krasiński <kontakt@arturkrasinski.pl>
2024-10-25 13:33:13 +0200

    feat: enable dynamic image URLs in component (#1839)
    Co-authored-by: jay-taylerson <87064668+jay-taylerson@users.noreply.github.com>

--- src/components/ReleaseCard.js
+++ src/components/ReleaseCard.js
@@ -1,24 +1,39 @@
-import React from "react";
-import {Card} from '@mui/material'
-import {ArrowForward} from '@mui/icons-material'
+import React, { useState, useEffect } from 'react';
+import { Card } from '@mui/material';
+import { ArrowForward } from '@mui/icons-material';
 
-export default function ReleaseCard({heading, blog_link, rn_link, img_url, alt_text}){
+export default function ReleaseCard({ heading, blog_link, rn_link, img_url, alt_text }) {
+  const [imageSrc, setImageSrc] = useState(null);
+
+  useEffect(() => {
+    const loadImage = async () => {
+      try {
+        const image = await import(`@site/static/img/${img_url}`);
+        setImageSrc(image.default);
+      } catch (err) {
+        console.error(`Error loading image: ${err}`);
+      }
+    };
+
+    loadImage();
+  }, [img_url]);
+
   return (
-    <Card sx={{backgroundColor: 'var(--warm-gray-4)', padding: '2%', display: 'flexbox', flexGrow: '1', flexDirection: 'row', margin: '1%'}} style={{height: '100%'}}>
-      <div style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
-        <h4 style={{display: 'flexbox', fontSize: '32px', fontWeight: 'bolder'}}>{heading}</h4>
-        <img src={img_url} alt={alt_text}/>
-        <p/>
-        <a href={blog_link} target="_blank" style={{display: 'flexbox', fontSize: '14px', fontWeight: 'bold'}}>
+    <Card sx={{ backgroundColor: 'var(--warm-gray-4)', padding: '2%', display: 'flexbox', flexGrow: '1', flexDirection: 'row', margin: '1%' }} style={{ height: '100%' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <h4 style={{ display: 'flexbox', fontSize: '32px', fontWeight: 'bolder' }}>{heading}</h4>
+        {imageSrc && <img src={imageSrc} alt={alt_text} />}
+        <p />
+        <a href={blog_link} target="_blank" style={{ display: 'flexbox', fontSize: '14px', fontWeight: 'bold' }}>
           Read the blog
-          <ArrowForward sx={{height: '10px', width: 'auto', paddingLeft: '1%'}}/>
+          <ArrowForward sx={{ height: '10px', width: 'auto', paddingLeft: '1%' }} />
         </a>
-        <p/>
-        <a href={rn_link} style={{display: 'flexbox', fontSize: '14px', fontWeight: 'bold'}}>
+        <p />
+        <a href={rn_link} style={{ display: 'flexbox', fontSize: '14px', fontWeight: 'bold' }}>
           View release notes
-          <ArrowForward sx={{height: '10px', width: 'auto', paddingLeft: '1%'}}/>
+          <ArrowForward sx={{ height: '10px', width: 'auto', paddingLeft: '1%' }} />
         </a>
       </div>
     </Card>
-  )
+  );
 }
