e9338e545a87d82a5a9445eaa5c7c1c78e993257
jay-taylerson <87064668+jay-taylerson@users.noreply.github.com>
2024-10-25 15:39:54 +0100

    Relase card width, sidebar width and general content filling full space (#1841)
    * Relase card width, sidebar width and general content filling full space
    
    * fix: fix image size (#1843)
    
    ---------
    
    Co-authored-by: jay.taylerson <james.taylerson@gmail.com>
    Co-authored-by: Artur Krasiński <kontakt@arturkrasinski.pl>

--- src/components/ReleaseCard.js
+++ src/components/ReleaseCard.js
@@ -19,10 +19,10 @@ export default function ReleaseCard({ heading, blog_link, rn_link, img_url, alt_
   }, [img_url]);
 
   return (
-    <Card sx={{ backgroundColor: 'var(--warm-gray-4)', padding: '2%', display: 'flexbox', flexGrow: '1', flexDirection: 'row', margin: '1%' }} style={{ height: '100%' }}>
+    <Card sx={{ backgroundColor: 'var(--warm-gray-4)', padding: '2%', display: 'flexbox', flexGrow: '1', flexDirection: 'row', margin: '1%' }} className='release-card' >
       <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
         <h4 style={{ display: 'flexbox', fontSize: '32px', fontWeight: 'bolder' }}>{heading}</h4>
-        {imageSrc && <img src={imageSrc} alt={alt_text} />}
+        {imageSrc && <img src={imageSrc} alt={alt_text} className="release-card-image"/>}
         <p />
         <a href={blog_link} target="_blank" style={{ display: 'flexbox', fontSize: '14px', fontWeight: 'bold' }}>
           Read the blog
