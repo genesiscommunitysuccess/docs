4a84af966f6437594601f1e2af18b8f51414aeaa
Matt Walker <matt.walker@genesis.global>
2024-10-30 05:26:21 +0000

    docs: genesis component example for checkbox PTL-1569

--- examples/ui/client-capabilities/interaction/checkbox.js
+++ examples/ui/client-capabilities/interaction/checkbox.js
@@ -3,5 +3,5 @@ import { registerComponents } from '../../rapidImports';
 registerComponents();
 
 export default function CheckboxDemo({ children, color }) {
-	return (<rapid-checkbox></rapid-checkbox>)
+	return (<div><label id='checkbox' style={{ paddingRight: '5px' }}>Checkbox</label><rapid-checkbox aria-labelledby='checkbox'></rapid-checkbox></div >)
 }
