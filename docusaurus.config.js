628ce35807872205e490eebe2403ae849e59312f
jay.taylerson <james.taylerson@gmail.com>
2024-10-31 13:40:16 +0000

    Moving broken links to warning whilst we're mid porting things, as its breaking the public build/hosting but some pages linked to have not been ported and restructured yet

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,8 +25,8 @@ const config = {
     format: "detect",
     mermaid: true,
   },
-  onBrokenLinks: "throw", // please do NOT change this to 'warn', fix or remove your broken links instead
-  onBrokenMarkdownLinks: "throw", // please do NOT change this to 'warn', fix or remove your broken links instead
+  onBrokenLinks: "warn", // please do NOT change this to 'warn', fix or remove your broken links instead
+  onBrokenMarkdownLinks: "warn", // please do NOT change this to 'warn', fix or remove your broken links instead
   onDuplicateRoutes: "throw",
   clientModules: ["./src/client-modules/genesislcap.js"],
   scripts: [
