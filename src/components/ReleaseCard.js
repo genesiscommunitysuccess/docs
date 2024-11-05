94c261fd3a9318fb4ff0db28057e2ac6ab51184f
ArturKrasinski <kontakt@arturkrasinski.pl>
2024-11-05 13:40:04 +0100

    feat: add CardList component

--- src/components/ReleaseCard.js
+++ src/components/ReleaseCard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { Card } from '@mui/material';
 import { ArrowForward } from '@mui/icons-material';
 
