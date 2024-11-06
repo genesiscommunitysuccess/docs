c3340a12ba6747946aab09fa5017ddfffcc1f846
Gareth Spencer <25119104+Gareth-Spencer-Genesis@users.noreply.github.com>
2024-11-06 09:55:22 +0000

    docs: ui capabilities text area PTL-1580

--- examples/ui/client-capabilities/interaction/text-area.js
+++ examples/ui/client-capabilities/interaction/text-area.js
@@ -4,5 +4,5 @@ import { registerComponents } from '../../rapidImports';
 registerComponents();
 
 export default function TextAreaDemo({ children, color }) {
-	return (<CodeSection><rapid-text-area autocapitalize="sentences" placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua">Text Area</rapid-text-area></CodeSection >)
+	return (<CodeSection><rapid-text-area maxlength="20" placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" rows="10" cols="10">Text Area</rapid-text-area></CodeSection >)
 }
