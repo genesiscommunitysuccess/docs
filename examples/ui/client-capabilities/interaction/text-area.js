04b20b395e5bb02b18a611ee51b39828bef36783
Gareth Spencer <25119104+Gareth-Spencer-Genesis@users.noreply.github.com>
2024-11-06 18:23:46 +0000

    docs: ui capabilities text area PTL-1580

--- examples/ui/client-capabilities/interaction/text-area.js
+++ examples/ui/client-capabilities/interaction/text-area.js
@@ -4,5 +4,14 @@ import { registerComponents } from '../../rapidImports';
 registerComponents();
 
 export default function TextAreaDemo({ children, color }) {
-	return (<CodeSection><rapid-text-area maxlength="20" placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" rows="10" cols="10">Text Area</rapid-text-area></CodeSection >)
+	return (
+		<CodeSection>
+			<div style={{ color: 'var(--neutral-foreground-rest)', width: '-webkit-fill-available'}}>
+				<div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', flexDirection: 'column', }}>
+					<rapid-text-area maxlength='20' placeholder='Try to type more than 20 characters.' rows='3' cols='10'>Sample text area with maxlength 20 and three rows.</rapid-text-area>
+					<rapid-text-area resize='both' placeholder='Drag text are from bottom right to resize.'>Sample text area with resize both set</rapid-text-area>
+				</div>
+			</div>
+		</CodeSection>
+	)
 }
