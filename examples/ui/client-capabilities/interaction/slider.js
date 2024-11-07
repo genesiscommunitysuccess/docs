7b2666bd0c89ce2f96780acc2d318fafb4ac5794
Gareth Spencer <25119104+Gareth-Spencer-Genesis@users.noreply.github.com>
2024-11-07 10:01:54 +0000

    docs: ui capabilities slider PTL-1577

--- examples/ui/client-capabilities/interaction/slider.js
+++ examples/ui/client-capabilities/interaction/slider.js
@@ -4,5 +4,16 @@ import { registerComponents } from '../../rapidImports';
 registerComponents();
 
 export default function SliderDemo({ children, color }) {
-	return (<CodeSection><CodeLabel>Slider:</CodeLabel><rapid-slider></rapid-slider></CodeSection >)
+	return (
+		<CodeSection>
+			<CodeLabel>Slider:</CodeLabel>
+			<div style={{ height:'100px', width:'100%', padding:'30px'}}>
+				<rapid-slider min="0" max="100" step="10" value="50">
+					<rapid-slider-label position="0"> Low </rapid-slider-label>
+					<rapid-slider-label position="50"> Mid </rapid-slider-label>
+					<rapid-slider-label position="100"> High </rapid-slider-label>
+				</rapid-slider>
+			</div>
+		</CodeSection >
+	)
 }
