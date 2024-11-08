11e0417ce818cfac2c6aca2410d693261e4a6a8e
Gareth Spencer <25119104+Gareth-Spencer-Genesis@users.noreply.github.com>
2024-11-08 16:28:10 +0000

    docs: ui capabilities radio PTL-1575

--- examples/ui/client-capabilities/interaction/radio.js
+++ examples/ui/client-capabilities/interaction/radio.js
@@ -1,9 +1,15 @@
 import { CodeLabel, CodeSection } from '../../documentationBase';
-import { registerComponents } from '../../rapidImports';
-
-registerComponents();
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 export default function RadioDemo({ children, color }) {
+
+	const isBrowser = useIsBrowser();
+
+	if (isBrowser) {
+		const RapidImports = require('../../rapidImports');
+		RapidImports.registerComponents();
+	}
+
 	return (
 		<CodeSection>
 			<rapid-radio>Radio</rapid-radio>
