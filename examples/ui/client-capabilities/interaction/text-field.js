bf6124f3be3b771e902a2e3cb3a00f554998848d
Gareth Spencer <25119104+Gareth-Spencer-Genesis@users.noreply.github.com>
2024-11-08 16:31:25 +0000

    docs: ui capabilities text field PTL-1581

--- examples/ui/client-capabilities/interaction/text-field.js
+++ examples/ui/client-capabilities/interaction/text-field.js
@@ -1,8 +1,15 @@
 import { CodeLabel, CodeSection } from '../../documentationBase';
-import { registerComponents } from '../../rapidImports';
-
-registerComponents();
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 export default function TextFieldDemo({ children, color }) {
+
+	const isBrowser = useIsBrowser();
+
+	if (isBrowser) {
+		const RapidImports = require('../../rapidImports');
+		RapidImports.registerComponents();
+	}
+
 	return (<CodeSection><rapid-text-field placeholder="Input text here">Sample Text Field</rapid-text-field></CodeSection >)
 }
+
