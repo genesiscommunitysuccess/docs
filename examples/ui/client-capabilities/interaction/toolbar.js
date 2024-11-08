0a1642896d89842ac5a1987594a2de19d416396e
Gareth Spencer <25119104+Gareth-Spencer-Genesis@users.noreply.github.com>
2024-11-08 16:22:55 +0000

    docs: ui capabilities toolbar PTL-1582

--- examples/ui/client-capabilities/interaction/toolbar.js
+++ examples/ui/client-capabilities/interaction/toolbar.js
@@ -1,9 +1,17 @@
-import { CodeLabel, CodeSection } from '../../documentationBase';
-import { registerComponents } from '../../rapidImports';
+import { CodeSection } from '../../documentationBase';
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 registerComponents();
 
 export default function ToolbarDemo({ children, color }) {
+
+	const isBrowser = useIsBrowser();
+
+	if (isBrowser) {
+		const RapidImports = require('../../rapidImports');
+		RapidImports.registerComponents();
+	}
+
 	return (
 		<CodeSection>
 			<rapid-toolbar>
