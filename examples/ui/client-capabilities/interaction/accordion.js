7ce4f8a8f11d952146dc464153d9b6efd2be130d
Matt Walker <matt.walker@genesis.global>
2024-11-18 15:28:18 +0000

    docs: accordion examples and live demo PTL-1587

---
+++ examples/ui/client-capabilities/interaction/accordion.js
@@ -0,0 +1,30 @@
+import { CodeSection } from '../../documentationBase';
+import useIsBrowser from '@docusaurus/useIsBrowser';
+
+export default function AccordionDemo({ children, color }) {
+
+  const isBrowser = useIsBrowser();
+
+	if (isBrowser) {
+		const RapidImports = require('../../rapidImports');
+		RapidImports.registerComponents();
+	}
+
+	return (
+		<CodeSection>
+			<rapid-accordion>
+				<rapid-accordion-item expanded>
+						<span slot="heading">Panel one</span>
+						Panel one content
+				</rapid-accordion-item>
+				<rapid-accordion-item>
+						<span slot="heading">Panel two</span>
+						Panel two content
+				</rapid-accordion-item>
+				<rapid-accordion-item expanded>
+						<span slot="heading">Panel three</span>
+						Panel three content
+				</rapid-accordion-item>
+			</rapid-accordion>
+		</CodeSection>)
+}
