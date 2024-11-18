2af55950ef55c1e9d948fb10d008e810a99e5515
Matt Walker <matt.walker@genesis.global>
2024-11-18 17:41:24 +0000

    docs: pr comments PTL-1587

--- examples/ui/client-capabilities/interaction/accordion.js
+++ examples/ui/client-capabilities/interaction/accordion.js
@@ -12,19 +12,21 @@ export default function AccordionDemo({ children, color }) {
 
 	return (
 		<CodeSection>
-			<rapid-accordion>
-				<rapid-accordion-item expanded>
-						<span slot="heading">Panel one</span>
-						Panel one content
-				</rapid-accordion-item>
-				<rapid-accordion-item>
-						<span slot="heading">Panel two</span>
-						Panel two content
-				</rapid-accordion-item>
-				<rapid-accordion-item expanded>
-						<span slot="heading">Panel three</span>
-						Panel three content
-				</rapid-accordion-item>
-			</rapid-accordion>
+			<div style={{ color: 'var(--neutral-foreground-rest)', width: '-webkit-fill-available', height: '180px'}}>
+				<rapid-accordion>
+					<rapid-accordion-item expanded>
+							<span slot="heading">Panel one</span>
+							Panel one content
+					</rapid-accordion-item>
+					<rapid-accordion-item>
+							<span slot="heading">Panel two</span>
+							Panel two content
+					</rapid-accordion-item>
+					<rapid-accordion-item expanded>
+							<span slot="heading">Panel three</span>
+							Panel three content
+					</rapid-accordion-item>
+				</rapid-accordion>
+			</div>
 		</CodeSection>)
 }
