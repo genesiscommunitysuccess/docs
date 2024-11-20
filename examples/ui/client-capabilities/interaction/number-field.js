361f53fc6751db75f15d7f4b9af4589ce933f6d1
Gareth Spencer <25119104+Gareth-Spencer-Genesis@users.noreply.github.com>
2024-11-20 15:35:04 +0000

    docs: ui capabilities number field PTL-1571

---
+++ examples/ui/client-capabilities/interaction/number-field.js
@@ -0,0 +1,24 @@
+import { CodeSection } from '../../documentationBase';
+import useIsBrowser from '@docusaurus/useIsBrowser';
+
+function setup() {
+	// Setup
+	const isBrowser = useIsBrowser();
+	if (isBrowser) {
+		const RapidImports = require('../../rapidImports');
+		RapidImports.registerComponents();
+	}
+}
+
+export default function NumberFieldDemo({ children, color }) {
+	setup();
+	return (
+		<CodeSection>
+			<div style={{ color: 'var(--neutral-foreground-rest)', width: '-webkit-fill-available'}}>
+				<div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', flexDirection: 'column', }}>
+					<rapid-number-field value="1"></rapid-number-field>
+				</div>
+			</div>
+		</CodeSection>
+	)
+}
