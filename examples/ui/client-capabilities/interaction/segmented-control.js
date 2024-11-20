209ed064036ac05d9d21bd266eb46c01faac2cd5
Gareth Spencer <25119104+Gareth-Spencer-Genesis@users.noreply.github.com>
2024-11-20 13:46:48 +0000

    docs: ui capabilities button segmentation PTL-1585

--- examples/ui/client-capabilities/interaction/segmented-control.js
+++ examples/ui/client-capabilities/interaction/segmented-control.js
@@ -1,7 +1,6 @@
 import { CodeSection } from '../../documentationBase';
 import useIsBrowser from '@docusaurus/useIsBrowser';
-import React, { useEffect, useRef, useState } from 'react';
-import {Serialisers} from "@genesislcap/foundation-criteria";
+import React, { useRef, useState } from 'react';
 export default function SegmentedControlDemo({ children, color }) {
 
 	const isBrowser = useIsBrowser();
@@ -15,10 +14,6 @@ export default function SegmentedControlDemo({ children, color }) {
 
 	const [outputValue, setOutputValue] = useState('');
 
-	useEffect(() => {
-
-	}, []);
-
 	const handleChange = (event) => {
 		setOutputValue(event.target.value);
 	}
@@ -28,10 +23,15 @@ export default function SegmentedControlDemo({ children, color }) {
 			<div style={{ color: 'var(--neutral-foreground-rest)', width: '-webkit-fill-available'}}>
 				<div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', flexDirection: 'column', }}>
 					<rapid-segmented-control ref={segmentedControlRef} onClick={handleChange}>
-						<rapid-segmented-item>Item 1</rapid-segmented-item>
-						<rapid-segmented-item>Item 2</rapid-segmented-item>
-						<rapid-segmented-item>Item 3</rapid-segmented-item>
+						<rapid-segmented-item value="1">Item 1</rapid-segmented-item>
+						<rapid-segmented-item value="2">Item 2</rapid-segmented-item>
+						<rapid-segmented-item value="3">Item 3</rapid-segmented-item>
 					</rapid-segmented-control>
+					<rapid-text-field style={{ width: '180px', }}
+						readOnly
+						value={outputValue}>
+						Chosen Option:
+					</rapid-text-field>
 				</div>
 			</div>
 		</CodeSection>
