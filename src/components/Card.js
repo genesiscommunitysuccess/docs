e51b7f8549838b74b738a08551398c20b4db7902
ArturKrasinski <kontakt@arturkrasinski.pl>
2024-11-20 11:34:18 +0100

    Card list - example for server capabilities

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export default function Card({ heading, link, text }) {
     <a href={link} title={heading} className="card">
       <CardMaterial 
         sx={{ backgroundColor: 'var(--white)', position: 'relative', padding: '32px', display: 'flexbox', flexGrow: '1', flexDirection: 'row', margin: '1%', borderRadius: "8px" }}
-        style={{ height: '100%', boxShadow: "0 0 1px 0 rgba(0,0,0,.35),0 20px 40px 0 rgba(10,0,74,.07)" }}
+        style={{ height: '100%', boxShadow: 'none' }}
       >
         <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
           <h4 style={{ display: 'flexbox', fontSize: '26px', fontWeight: '400', fontFamily: "Aeonik" }}>{heading}</h4>
