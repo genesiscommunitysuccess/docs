842e2c1573a1753eda2bcecab2b966b2e489b4d2
Matt Walker <matt.walker@genesis.global>
2024-11-22 15:29:21 +0000

    docs: update live example to react 19 format PTL-1526

--- examples/ui/client-capabilities/filters/filters.js
+++ examples/ui/client-capabilities/filters/filters.js
@@ -1,5 +1,5 @@
 import { CodeSection } from '../../documentationBase';
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import useIsBrowser from '@docusaurus/useIsBrowser';
 import { timeWindowFilter } from '@genesislcap/foundation-filters';
 
@@ -8,45 +8,44 @@ const oneDay = 24*60*60*1000;
 const createDateFormat = (relativeTime = 0) => new Date(Date.now() + relativeTime).toISOString().slice(0,16)
 
 export default function FiltersDemo({ children, color }) {
+  const isBrowser = useIsBrowser();
 
-	const isBrowser = useIsBrowser();
+  if (isBrowser) {
+    const RapidImports = require('../../rapidImports');
+    RapidImports.registerComponents();
+  }
 
-	if (isBrowser) {
-		const RapidImports = require('../../rapidImports');
-		RapidImports.registerComponents();
-	}
+  const [outputValue, setOutputValue] = useState('');
+  const [startVal, setStartVal] = useState(createDateFormat(-1 * oneDay));
+  const [endVal, setEndVal] = useState(createDateFormat(oneDay));
 
-	const startDateRef = useRef(null);
-    const endDateRef = useRef(null);
-	const [outputValue, setOutputValue] = useState('');
-	const [startVal, setStartVal] = useState('');
+  const checkTimeWindow = () =>
+    setOutputValue(timeWindowFilter(startVal, endVal)
+      ? "Current Date/Time exists within the start and end period"
+      : "Current Date/Time doesn't fall between start and end period")
 
-    const checkTimeWindow = () => {
-		if(startDateRef.current.value && endDateRef.current.value) {
-			setOutputValue(timeWindowFilter(startDateRef.current.value, endDateRef.current.value)? 'Current Date/Time exists within the start and end period': "Current Date/Time doesn't fall between start and end period");
-		}
-    }
+  const handleTextFieldChanged = (callback) => (event) => callback(event.target.value);
 
-	useEffect(() => {
-		if (startDateRef.current) {
-		  startDateRef.current.value = createDateFormat(-1 * oneDay);
-		}
-
-		if(endDateRef.current) {
-			endDateRef.current.value = createDateFormat(oneDay);
-		}
-	  }, []);
-
-	return (
-		<CodeSection>
-			<div style={{ color: 'var(--neutral-foreground-rest)', width: '-webkit-fill-available'}}>
-				<div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', flexDirection: 'column', }}>
-                    <rapid-text-field  type="datetime-local" ref={startDateRef}> Start Period</rapid-text-field>
-                    <rapid-text-field  type="datetime-local" ref={endDateRef}>End Period</rapid-text-field>
-                    <rapid-button onClick={checkTimeWindow}>Check</rapid-button>
-                    <span style={{ height: '20px' }}>{outputValue}</span>
-				</div>
-			</div>
-		</CodeSection>
-	)
+  return (
+    <CodeSection>
+      <div style={{ color: 'var(--neutral-foreground-rest)', width: '-webkit-fill-available'}}>
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', flexDirection: 'column', }}>
+          <rapid-text-field
+            type="datetime-local"
+            value={startVal}
+            onInput={handleTextFieldChanged(setStartVal)}>
+            Start Period
+          </rapid-text-field>
+          <rapid-text-field
+            type="datetime-local"
+            value={endVal}
+            onInput={handleTextFieldChanged(setEndVal)}>
+            End Period
+          </rapid-text-field>
+          <rapid-button onClick={checkTimeWindow}>Check</rapid-button>
+          <span style={{ height: '20px' }}>{outputValue}</span>
+        </div>
+      </div>
+    </CodeSection>
+  )
 }
