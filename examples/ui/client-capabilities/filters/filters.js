83ad2c2e88a10a7570a40a555620f0cff176b71b
mahamzafar990 <maham.zafar@genesis.global>
2024-11-22 12:38:31 +0000

    chore: addressed PR feedback

--- examples/ui/client-capabilities/filters/filters.js
+++ examples/ui/client-capabilities/filters/filters.js
@@ -1,5 +1,5 @@
-import { CodeLabel, CodeSection } from '../../documentationBase';
-import React, { useRef } from 'react';
+import { CodeSection } from '../../documentationBase';
+import React, { useRef, useState } from 'react';
 import useIsBrowser from '@docusaurus/useIsBrowser';
 import { timeWindowFilter } from '@genesislcap/foundation-filters';
 export default function FiltersDemo({ children, color }) {
@@ -13,10 +13,12 @@ export default function FiltersDemo({ children, color }) {
 
 	const startDateRef = useRef(null);
     const endDateRef = useRef(null);
-    const result = useRef(null);
+	const [outputValue, setOutputValue] = useState('');
 
     const checkTimeWindow = () => {
-        result.current.textContent = timeWindowFilter(startDateRef.current.value, endDateRef.current.value)? 'Current Date/Time exists within the start and end period': "Current Date/Time doesn't fall between start and end period";
+		if(startDateRef.current.value && endDateRef.current.value) {
+			setOutputValue(timeWindowFilter(startDateRef.current.value, endDateRef.current.value)? 'Current Date/Time exists within the start and end period': "Current Date/Time doesn't fall between start and end period");
+		}
     }
 
 	return (
@@ -25,8 +27,8 @@ export default function FiltersDemo({ children, color }) {
 				<div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', flexDirection: 'column', }}>
                     <rapid-text-field  type="datetime-local" ref={startDateRef}> Start Period</rapid-text-field>
                     <rapid-text-field  type="datetime-local" ref={endDateRef}>End Period</rapid-text-field>
-                    <rapid-button onClick={() => checkTimeWindow()}>Check</rapid-button>
-                    <span style={{ height: '20px' }} ref={result} ></span>
+                    <rapid-button onClick={checkTimeWindow}>Check</rapid-button>
+                    <span style={{ height: '20px' }}>{outputValue}</span>
 				</div>
 			</div>
 		</CodeSection>
