f75b2757b51b9f2a4a598d76b3c787febd554801
ArturKrasinski <kontakt@arturkrasinski.pl>
2024-11-25 19:16:11 +0100

    docs: PTL-1509

---
+++ docs/001_develop/03_client-capabilities/004_header/examples/primitive_header_example.js
@@ -0,0 +1,50 @@
+import { CodeSection } from "@site/examples/ui/documentationBase";
+import useIsBrowser from "@docusaurus/useIsBrowser";
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import './primitive_header_example.css';
+
+export default function PrimitiveHeaderExample() {
+  const isBrowser = useIsBrowser();
+
+  if (isBrowser) {
+    const RapidImports = require("@site/examples/ui/rapidImports");
+    RapidImports.registerComponents();
+  }
+
+  const logoUrl = useBaseUrl('/img/logo-icon--light.svg');
+
+  const navItems = [{
+    title: "Home",
+    icon: {
+      name: "home",
+      variant: "solid",
+    },
+    navId: "header",
+    placementIndex: 0,
+    routePath: "home",
+    routeName: "home",
+  },
+  {
+    title: "Grids",
+    icon: {
+        name: "table",
+        variant: "solid",
+      },
+    navId: "header",
+    placementIndex: 10,
+    routePath: "grids",
+    routeName: "grids",
+  }];
+
+
+  return (
+    <CodeSection>
+      <foundation-header
+        className="primitive-header-example"
+        logo-src={logoUrl}
+        style={{ width: "100%" }}
+        routeNavItems={navItems}
+      ></foundation-header>
+    </CodeSection>
+  );
+}
