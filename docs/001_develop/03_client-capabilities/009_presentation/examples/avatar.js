d44ee09ed09ac98863fdfe4263007ab57e628e0a
Gareth Spencer <25119104+Gareth-Spencer-Genesis@users.noreply.github.com>
2024-11-25 09:06:26 +0000

    docs: ui capabilities avatar PTL-1600

--- docs/001_develop/03_client-capabilities/009_presentation/examples/avatar.js
+++ docs/001_develop/03_client-capabilities/009_presentation/examples/avatar.js
@@ -15,7 +15,7 @@ export default function AvatarDemo({ children, color }) {
 	return (
 		<CodeSection>
 			<div style={{ color: 'var(--neutral-foreground-rest)', width: '-webkit-fill-available'}}>
-				<div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', width: '50x', flexDirection: 'column', }}>
+				<div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', width: '50px', flexDirection: 'column', }}>
 					<rapid-avatar shape="circle" fill="green" color="black">JD</rapid-avatar>
 				</div>
 			</div>
