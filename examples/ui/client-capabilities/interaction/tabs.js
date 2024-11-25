1759014801fb8ff05e2858f620f41330b9da5482
Matt Walker <matt.walker@genesis.global>
2024-11-25 15:19:52 +0000

    docs: tabs live example PTL-1596

---
+++ examples/ui/client-capabilities/interaction/tabs.js
@@ -0,0 +1,54 @@
+import { CodeSection } from '../../documentationBase';
+import useIsBrowser from '@docusaurus/useIsBrowser';
+import { MenuItem } from '@genesislcap/rapid-design-system';
+
+function ListItem({children}) {
+	return (<li style={{color: 'white'}}>{children}</li>)
+}
+
+export default function MenuDemo({ children, color }) {
+
+	const isBrowser = useIsBrowser();
+
+	if (isBrowser) {
+		const RapidImports = require('../../rapidImports');
+		RapidImports.registerComponents();
+	}
+
+	const style = {
+		marginRight: '4px',
+		marginLeft: '4px',
+	};
+
+	const handleChange = (e) => {
+		console.log(e.target)
+	}
+
+	return (
+		<CodeSection>
+			<rapid-tabs activeid="entrees">
+				<rapid-tab id="apps">Appetizers</rapid-tab>
+				<rapid-tab id="entrees">Entrees</rapid-tab>
+				<rapid-tab id="desserts">Desserts</rapid-tab>
+				<rapid-tab-panel id="appsPanel">
+					<ol>
+						<ListItem>Stuffed artichokes</ListItem>
+						<ListItem>Bruschetta</ListItem>
+					</ol>
+				</rapid-tab-panel>
+				<rapid-tab-panel id="entreesPanel">
+					<ol>
+						<ListItem>Tomato Bread Soup with Steamed Mussels</ListItem>
+						<ListItem>Grilled Fish with Artichoke Caponata</ListItem>
+					</ol>
+				</rapid-tab-panel>
+				<rapid-tab-panel id="dessertsPanel">
+					<ol>
+						<ListItem>Tiramisu</ListItem>
+						<ListItem>ListItemmoncello and Ice Cream with Biscotti</ListItem>
+					</ol>
+				</rapid-tab-panel>
+			</rapid-tabs>
+		</CodeSection >
+	)
+}
