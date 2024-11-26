65b0553e44a3e78ec7ccd77c39c1bb82c505048f
Gareth Spencer <25119104+Gareth-Spencer-Genesis@users.noreply.github.com>
2024-11-26 17:33:48 +0000

    docs: ui capabilities icon PTL-1611

--- docs/001_develop/03_client-capabilities/009_presentation/examples/icon.js
+++ docs/001_develop/03_client-capabilities/009_presentation/examples/icon.js
@@ -15,13 +15,15 @@ export default function IconDemo({ children, color }) {
 	setup();
 	return (
 		<CodeSection>
-			<div style={{ color: 'var(--neutral-foreground-rest)', width: '-webkit-fill-available'}}>
-				<div style={{ display: 'flex', flexWrap: 'wrap', height: '75px', width: '100%', flexDirection: 'column', }}>
-					<div>
+			<div style={{ color: 'var(--neutral-foreground-rest)' }}>
+				<div style={{ display: 'flex', flexWrap: 'wrap', height: '30px', width: '300px', flexDirection: 'column', }}>
+					<div style={{ display: 'flex', flexWrap: 'wrap', width: '30px' }}>
 						<rapid-icon name="house"></rapid-icon>
-						<rapid-icon name="user" variant="regular"></rapid-icon>
-						<rapid-icon name="github" variant="brand"></rapid-icon>
 					</div>
+					<div style={{ display: 'flex', flexWrap: 'wrap', width: '30px' }}>
+						<rapid-icon name="user" variant="regular"></rapid-icon>
+					</div>
+						<rapid-icon name="github" variant="brand"></rapid-icon>
 				</div>
 			</div>
 		</CodeSection>
