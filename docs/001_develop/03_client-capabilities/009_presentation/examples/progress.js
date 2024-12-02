da1e5b791495cef9fd696274c000666959cd1d8b
Gareth Spencer <25119104+Gareth-Spencer-Genesis@users.noreply.github.com>
2024-12-02 08:56:19 +0000

    fix: replace -webkit-fill-available (PTL-1568)

--- docs/001_develop/03_client-capabilities/009_presentation/examples/progress.js
+++ docs/001_develop/03_client-capabilities/009_presentation/examples/progress.js
@@ -15,9 +15,9 @@ export default function ProgressDemo({ children, color }) {
 	setup();
 	return (
 		<CodeSection>
-			<div style={{ color: 'var(--neutral-foreground-rest)', width: '-webkit-fill-available'}}>
+			<div style={{ color: 'var(--neutral-foreground-rest)', width: '100%'}}>
 				<div style={{ display: 'flex', flexWrap: 'wrap', width: '100%', flexDirection: 'row', justifyContent: 'space-evenly' }}>
-					<rapid-progress style={{ width: '-webkit-fill-available'}} min="0" max="100" ></rapid-progress>
+					<rapid-progress style={{ width: '100%'}} min="0" max="100" ></rapid-progress>
 				</div>
 			</div>
 		</CodeSection>
