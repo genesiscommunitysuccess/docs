03b7302a8a764fd6f7d7b8de416272d873b64ed5
ArturKrasinski <kontakt@arturkrasinski.pl>
2024-12-03 11:10:21 +0100

    feat: new release card design

--- src/components/ReleaseCard/ReleaseCard.js
+++ src/components/ReleaseCard/ReleaseCard.js
@@ -1,17 +1,21 @@
-import Card from '@site/src/components/Card/Card';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import { ArrowForward } from '@mui/icons-material';
+import Card from '@site/src/components/Card/Card';
 import './ReleaseCard.css'
 
+const RELEASE_PAGE_URL_PREFIX = '/release-notes/platform';
+
 export default function ReleaseCard({ blogLink, releaseNotesLink, imageName, imageAltText }) {
+  const relativeReleaseNoteLink = useBaseUrl(`${RELEASE_PAGE_URL_PREFIX}${releaseNotesLink}`);
   const imageSrc = imageName ? `/img/release-notes/${imageName}` : null;
   
   const footerContent = (
     <nav className="release-card-footer">
-      <a href={blogLink} target="_blank" className="release-card-button">
+      <a href={blogLink} target="_blank" title="Blog" className="release-card-button">
         Blog
         <ArrowForward sx={{ height: '10px', width: 'auto', paddingLeft: '1%' }} />
       </a>
-      <a href={releaseNotesLink} className="release-card-button">
+      <a href={relativeReleaseNoteLink} title="Release notes" className="release-card-button">
         Release notes
         <ArrowForward sx={{ height: '10px', width: 'auto', paddingLeft: '1%' }} />
       </a>
