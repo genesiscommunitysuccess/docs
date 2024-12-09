9d7f60a5e59b8be2927478990c724a1af8b787b1
brycroftgenesis <104829364+brycroftgenesis@users.noreply.github.com>
2024-12-09 09:26:01 +0000

    chore: adding a script to add swap for the build (#2049)

---
+++ bootstrap-build-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Setting up swap space..."
+sudo fallocate -l 45G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon -s
+echo "✅  Done setting up swap space!"
+echo ""
