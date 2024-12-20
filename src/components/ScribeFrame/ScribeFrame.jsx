40fe165807b3b5c12954516de2c507413ca583ec
jay.taylerson <james.taylerson@gmail.com>
2024-12-20 11:52:19 +0000

    Adding more stand out styling

--- src/components/ScribeFrame/ScribeFrame.jsx
+++ src/components/ScribeFrame/ScribeFrame.jsx
@@ -4,6 +4,12 @@ import './ScribeFrame.css';
 export default function ScribeFrame({ src, title }) {
   return (
     <div className="iframeContainer">
+      <div class="learn-title">
+        <img src="./static/svg/learn.svg" alt="learn" class="learn-icon"></img>
+        <p class="sbs-title">
+          Learn step-by-step
+        </p>
+      </div>
       <iframe
         src={src}
         title={title}
