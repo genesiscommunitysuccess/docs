0be748604012f2cba08827aa404e755bbdb015f1
brycroftgenesis <104829364+brycroftgenesis@users.noreply.github.com>
2025-02-13 09:18:33 +0000

    Update bootstrap-build-env.sh (#2119)

--- bootstrap-build-env.sh
+++ bootstrap-build-env.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 echo "Setting up swap space..."
-sudo fallocate -l 45G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
+sudo fallocate -l 45G ../swapfile
+sudo chmod 600 ../swapfile
+sudo mkswap ../swapfile
+sudo swapon ../swapfile
 sudo swapon -s
 echo "✅  Done setting up swap space!"
 echo ""
