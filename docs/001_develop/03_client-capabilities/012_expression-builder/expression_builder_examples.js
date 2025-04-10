54d662e1179bbf916468455bd2efb3750fea54a7
Matt Walker <matt.walker@genesis.global>
2025-04-10 09:57:11 +0100

    docs: add base model to expression builder docs RPT-0

--- docs/001_develop/03_client-capabilities/012_expression-builder/expression_builder_examples.js
+++ docs/001_develop/03_client-capabilities/012_expression-builder/expression_builder_examples.js
@@ -1,6 +1,6 @@
 import { CodeSection } from '../../../../examples/ui/documentationBase';
 import useIsBrowser from '@docusaurus/useIsBrowser';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const EXAMPLE_MIN_AGE = 18;
 
@@ -63,43 +63,115 @@ export const fields = [
   },
 ];
 
+const model = {
+  TYPE: "PREDICATE_EXPRESSION",
+  OPERATION: "OR",
+  EXPRESSIONS: [
+    {
+      TYPE: "BINARY_EXPRESSION",
+      LEFT: {
+        TYPE: "FIELD",
+        NAME: "age",
+      },
+      OPERATION: "GREATER_THAN",
+      RIGHT: {
+        TYPE: "NUMBER",
+        VALUE: "18",
+      },
+    },
+    {
+      TYPE: "PREDICATE_EXPRESSION",
+      OPERATION: "AND",
+      EXPRESSIONS: [
+        {
+          TYPE: "BINARY_EXPRESSION",
+          LEFT: {
+            TYPE: "FIELD",
+            NAME: "country",
+          },
+          OPERATION: "EQUALS",
+          RIGHT: {
+            TYPE: "STRING",
+            VALUE: "uk",
+          },
+        },
+        {
+          TYPE: "BINARY_EXPRESSION",
+          LEFT: {
+            TYPE: "FIELD",
+            NAME: "isActive",
+          },
+          OPERATION: "EQUALS",
+          RIGHT: {
+            TYPE: "BOOLEAN",
+            VALUE: true,
+          },
+        },
+      ],
+    },
+  ],
+};
+
 export function RuleBuilderExample() {
-	const isBrowser = useIsBrowser();
-	const expressionBuilderRef = React.useRef(null);
+  const isBrowser = useIsBrowser();
+  const expressionBuilderRef = React.useRef(null);
 
-	const [modelString, setModelString] = useState({});
-	const [showModel, setShowModel] = useState(true);
+  const [modelString, setModelString] = useState(model);
+  const [showModel, setShowModel] = useState(true);
 
-	if (isBrowser) {
-		const RapidImports = require('../../../../examples/ui/rapidImports');
-		RapidImports.registerComponents();
-	}
+  if (isBrowser) {
+    const RapidImports = require('../../../../examples/ui/rapidImports');
+    RapidImports.registerComponents();
+  }
 
-	const change = (e) => {
-		setModelString(e.nativeEvent.detail)
-	}
+  const change = (e) => {
+    setModelString(e.nativeEvent.detail);
+  };
 
-	const ruleConfig = {
-		fields,
+  const ruleConfig = {
+    fields,
+    model,
     partialRuleValidationWarning: true,
-	}
+  };
 
-	// Use useEffect to set the property imperatively after the component is mounted
-	// but before the connected callback runs
-	React.useEffect(() => {
-		if (expressionBuilderRef.current) {
-			// Set the property directly on the element
-			expressionBuilderRef.current.ruleConfig = ruleConfig;
-		}
-	}, []);
+  // Use useEffect to set the property imperatively after the component is mounted
+  // but before the connected callback runs
+  React.useEffect(() => {
+    if (expressionBuilderRef.current) {
+      // Set the property directly on the element
+      expressionBuilderRef.current.ruleConfig = ruleConfig;
+    }
+  }, []);
 
-	return (
-		<CodeSection style={{flexDirection: 'column'}}>
-			<div style={{width: '100%', display: 'flex', justifyContent: 'flex-end'}}><rapid-button onClick={() => setShowModel(!showModel)}>{showModel ? 'Hide'  : 'Show'} Model</rapid-button></div>
-			<div style={{display: 'grid', gridTemplateColumns: showModel ? '2fr 1fr' : '1fr'}}>
-				<rapid-rule-expression-builder ref={expressionBuilderRef} onChange={change}></rapid-rule-expression-builder>
-				{showModel ? <pre style={{backgroundColor: '#292d3e', color: 'white', borderRadius: '6px'}}><code>{JSON.stringify(modelString, null, 2)}</code></pre> : null }
-			</div>
-		</CodeSection>
-	);
+  return (
+    <CodeSection style={{ flexDirection: 'column' }}>
+      <div style={{ 
+        width: '100%', 
+        display: 'flex', 
+        justifyContent: 'flex-end' 
+      }}>
+        <rapid-button onClick={() => setShowModel(!showModel)}>
+          {showModel ? 'Hide' : 'Show'} Model
+        </rapid-button>
+      </div>
+      <div style={{ 
+        display: 'grid', 
+        gridTemplateColumns: showModel ? '2fr 1fr' : '1fr' 
+      }}>
+        <rapid-rule-expression-builder 
+          ref={expressionBuilderRef} 
+          onChange={change}
+        ></rapid-rule-expression-builder>
+        {showModel ? (
+          <pre style={{ 
+            backgroundColor: '#292d3e', 
+            color: 'white', 
+            borderRadius: '6px' 
+          }}>
+            <code>{JSON.stringify(modelString, null, 2)}</code>
+          </pre>
+        ) : null}
+      </div>
+    </CodeSection>
+  );
 }
