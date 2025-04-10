792c0746f5ab31d94e945e2b36eada4c53341892
Matt Walker <matt.walker@genesis.global>
2025-04-10 12:06:45 +0100

    chore: fix formatting issues in code example RPT-0

--- docs/001_develop/03_client-capabilities/012_expression-builder/expression_builder_examples.js
+++ docs/001_develop/03_client-capabilities/012_expression-builder/expression_builder_examples.js
@@ -5,111 +5,111 @@ import React, { useState } from 'react';
 const EXAMPLE_MIN_AGE = 18;
 
 export const fields = [
-	{
-		fieldId: 'firstName',
-		label: 'First Name',
-		type: 'string',
-		input: 'text',
-		defaultValue: 'Matt',
-	},
-	{
-		fieldId: 'lastName',
-		label: 'Last Name',
-		type: 'string',
-		input: 'text',
-	},
-	{
-		fieldId: 'age',
-		label: 'Age',
-		type: 'int',
-		input: 'number',
-		validation: (age) =>
-			parseInt(age) < EXAMPLE_MIN_AGE ? `User must be at least ${EXAMPLE_MIN_AGE}` : null,
-	},
-	{
-		fieldId: 'weight',
-		label: 'Weight',
-		type: 'double',
-		input: 'number',
-	},
-	{
-		fieldId: 'isActive',
-		label: 'Is Active',
-		type: 'boolean',
-		input: 'checkbox',
-	},
-	{
-		fieldId: 'dateJoined',
-		label: 'Date Joined',
-		type: 'date',
-		input: 'date',
-	},
-	{
-		fieldId: 'country',
-		label: 'Country',
-		type: 'enum',
-		input: 'select',
-		values: {
-			usa: 'United States',
-			canada: 'Canada',
-			uk: 'United Kingdom',
-		},
-	},
-	{
-		fieldId: 'lastUpdated',
-		label: 'Last Updated',
-		type: 'date-time',
-		input: 'datetime-local',
-	},
+  {
+    fieldId: 'firstName',
+    label: 'First Name',
+    type: 'string',
+    input: 'text',
+    defaultValue: 'Matt',
+  },
+  {
+    fieldId: 'lastName',
+    label: 'Last Name',
+    type: 'string',
+    input: 'text',
+  },
+  {
+    fieldId: 'age',
+    label: 'Age',
+    type: 'int',
+    input: 'number',
+    validation: (age) =>
+      parseInt(age) < EXAMPLE_MIN_AGE ? `User must be at least ${EXAMPLE_MIN_AGE}` : null,
+  },
+  {
+    fieldId: 'weight',
+    label: 'Weight',
+    type: 'double',
+    input: 'number',
+  },
+  {
+    fieldId: 'isActive',
+    label: 'Is Active',
+    type: 'boolean',
+    input: 'checkbox',
+  },
+  {
+    fieldId: 'dateJoined',
+    label: 'Date Joined',
+    type: 'date',
+    input: 'date',
+  },
+  {
+    fieldId: 'country',
+    label: 'Country',
+    type: 'enum',
+    input: 'select',
+    values: {
+      usa: 'United States',
+      canada: 'Canada',
+      uk: 'United Kingdom',
+    },
+  },
+  {
+    fieldId: 'lastUpdated',
+    label: 'Last Updated',
+    type: 'date-time',
+    input: 'datetime-local',
+  },
 ];
 
 const ruleModel = {
-	TYPE: "PREDICATE_EXPRESSION",
-	OPERATION: "OR",
-	EXPRESSIONS: [
-		{
-			TYPE: "BINARY_EXPRESSION",
-			LEFT: {
-				TYPE: "FIELD",
-				NAME: "age",
-			},
-			OPERATION: "GREATER_THAN",
-			RIGHT: {
-				TYPE: "NUMBER",
-				VALUE: "18",
-			},
-		},
-		{
-			TYPE: "PREDICATE_EXPRESSION",
-			OPERATION: "AND",
-			EXPRESSIONS: [
-				{
-					TYPE: "BINARY_EXPRESSION",
-					LEFT: {
-						TYPE: "FIELD",
-						NAME: "country",
-					},
-					OPERATION: "EQUALS",
-					RIGHT: {
-						TYPE: "STRING",
-						VALUE: "uk",
-					},
-				},
-				{
-					TYPE: "BINARY_EXPRESSION",
-					LEFT: {
-						TYPE: "FIELD",
-						NAME: "isActive",
-					},
-					OPERATION: "EQUALS",
-					RIGHT: {
-						TYPE: "BOOLEAN",
-						VALUE: true,
-					},
-				},
-			],
-		},
-	],
+  TYPE: "PREDICATE_EXPRESSION",
+  OPERATION: "OR",
+  EXPRESSIONS: [
+    {
+      TYPE: "BINARY_EXPRESSION",
+      LEFT: {
+        TYPE: "FIELD",
+        NAME: "age",
+      },
+      OPERATION: "GREATER_THAN",
+      RIGHT: {
+        TYPE: "NUMBER",
+        VALUE: "18",
+      },
+    },
+    {
+      TYPE: "PREDICATE_EXPRESSION",
+      OPERATION: "AND",
+      EXPRESSIONS: [
+        {
+          TYPE: "BINARY_EXPRESSION",
+          LEFT: {
+            TYPE: "FIELD",
+            NAME: "country",
+          },
+          OPERATION: "EQUALS",
+          RIGHT: {
+            TYPE: "STRING",
+            VALUE: "uk",
+          },
+        },
+        {
+          TYPE: "BINARY_EXPRESSION",
+          LEFT: {
+            TYPE: "FIELD",
+            NAME: "isActive",
+          },
+          OPERATION: "EQUALS",
+          RIGHT: {
+            TYPE: "BOOLEAN",
+            VALUE: true,
+          },
+        },
+      ],
+    },
+  ],
 };
 
 export function RuleBuilderExample() {
@@ -170,14 +170,14 @@ export function RuleBuilderExample() {
 }
 
 const valueModel = {
-	TYPE: "METHOD_EXPRESSION",
-	PARAMETERS: [
-		{
-			TYPE: "FIELD",
-			NAME: "lastUpdated"
-		}
-	],
-	METHOD: "LONG_TO_DATE_TIME"
+  TYPE: "METHOD_EXPRESSION",
+  PARAMETERS: [
+    {
+      TYPE: "FIELD",
+      NAME: "lastUpdated"
+    }
+  ],
+  METHOD: "LONG_TO_DATE_TIME"
 };
 
 /**
@@ -188,72 +188,72 @@ const valueModel = {
  * @returns {Function} - A React component
  */
 const createExpressionBuilder = (componentType, initialModel, configPropName) => {
-	return function ExpressionBuilder() {
-		const isBrowser = useIsBrowser();
-		const expressionBuilderRef = React.useRef(null);
+  return function ExpressionBuilder() {
+    const isBrowser = useIsBrowser();
+    const expressionBuilderRef = React.useRef(null);
 
-		const [modelString, setModelString] = useState(initialModel);
-		const [showModel, setShowModel] = useState(true);
+    const [modelString, setModelString] = useState(initialModel);
+    const [showModel, setShowModel] = useState(true);
 
-		if (isBrowser) {
-			const RapidImports = require('../../../../examples/ui/rapidImports');
-			RapidImports.registerComponents();
-		}
+    if (isBrowser) {
+      const RapidImports = require('../../../../examples/ui/rapidImports');
+      RapidImports.registerComponents();
+    }
 
-		const change = (e) => {
-			setModelString(e.nativeEvent.detail);
-		};
+    const change = (e) => {
+      setModelString(e.nativeEvent.detail);
+    };
 
-		const config = {
-			fields,
-			model: initialModel,
-			partialRuleValidationWarning: true,
-		};
+    const config = {
+      fields,
+      model: initialModel,
+      partialRuleValidationWarning: true,
+    };
 
-		// Use useEffect to set the property imperatively after the component is mounted
-		// but before the connected callback runs
-		React.useEffect(() => {
-			if (expressionBuilderRef.current) {
-				// Set the property directly on the element
-				expressionBuilderRef.current[configPropName] = config;
-			}
-		}, []);
+    // Use useEffect to set the property imperatively after the component is mounted
+    // but before the connected callback runs
+    React.useEffect(() => {
+      if (expressionBuilderRef.current) {
+        // Set the property directly on the element
+        expressionBuilderRef.current[configPropName] = config;
+      }
+    }, []);
 
-		// Create the component element dynamically
-		const ExpressionBuilderElement = `rapid-${componentType}-expression-builder`;
+    // Create the component element dynamically
+    const ExpressionBuilderElement = `rapid-${componentType}-expression-builder`;
 
-		return (
-			<CodeSection style={{ flexDirection: 'column' }}>
-				<div style={{
-					width: '100%',
-					display: 'flex',
-					justifyContent: 'flex-end'
-				}}>
-					<rapid-button onClick={() => setShowModel(!showModel)}>
-						{showModel ? 'Hide' : 'Show'} Model
-					</rapid-button>
-				</div>
-				<div style={{
-					display: 'grid',
-					gridTemplateColumns: showModel ? '2fr 1fr' : '1fr'
-				}}>
-					{React.createElement(ExpressionBuilderElement, {
-						ref: expressionBuilderRef,
-						onChange: change
-					})}
-					{showModel ? (
-						<pre style={{
-							backgroundColor: '#292d3e',
-							color: 'white',
-							borderRadius: '6px'
-						}}>
-							<code>{JSON.stringify(modelString, null, 2)}</code>
-						</pre>
-					) : null}
-				</div>
-			</CodeSection>
-		);
-	};
+    return (
+      <CodeSection style={{ flexDirection: 'column' }}>
+        <div style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'flex-end'
+        }}>
+          <rapid-button onClick={() => setShowModel(!showModel)}>
+            {showModel ? 'Hide' : 'Show'} Model
+          </rapid-button>
+        </div>
+        <div style={{
+          display: 'grid',
+          gridTemplateColumns: showModel ? '2fr 1fr' : '1fr'
+        }}>
+          {React.createElement(ExpressionBuilderElement, {
+            ref: expressionBuilderRef,
+            onChange: change
+          })}
+          {showModel ? (
+            <pre style={{
+              backgroundColor: '#292d3e',
+              color: 'white',
+              borderRadius: '6px'
+            }}>
+              <code>{JSON.stringify(modelString, null, 2)}</code>
+            </pre>
+          ) : null}
+        </div>
+      </CodeSection>
+    );
+  };
 };
 
 // Create the specific builder components using the factory
