f189537f222c5aed645f608e2dd278f7c61fa138
mahamzafar990 <maham.zafar@genesis.global>
2025-04-14 17:07:38 +0100

    chore: updated docs PTL-1893

--- examples/ui/client-capabilities/interaction/scheduler.js
+++ examples/ui/client-capabilities/interaction/scheduler.js
@@ -10,12 +10,12 @@ export default function CronSchedulerDemo({ children, color }) {
 		const RapidImports = require('../../rapidImports');
 		RapidImports.registerComponents();
 	}
-	const [cron, setCron] = useState('0 30 10 ? * 1,3');
+	const [cron, setCron] = useState('0 30 10 ? * 2,4');
 
 	return (
 		<CodeSection>
-			<div style={{ color: 'var(--neutral-foreground-rest)', width: '100%', height: '300px'}}>
-				<rapid-scheduler-cron-builder cron="0 30 10 ? * 1,3" onChange={(e) => setCron(e.target.value)} >
+			<div style={{ color: 'var(--neutral-foreground-rest)', width: '100%', height: '350px'}}>
+				<rapid-scheduler-cron-builder cron="0 30 10 ? * 2,4" onChange={(e) => setCron(e.target.value)} >
 				</rapid-scheduler-cron-builder>
 				<p style={{ color: 'var(--neutral-foreground-rest)', fontSize: 'var(--type-ramp-base-font-size)', padding: 'calc(var(--design-unit) * 4px)'}}>Cron: {cron}</p>
 			</div>
