6c606223b4dac58a992fbc65e6958a9c40669c72
mahamzafar990 <maham.zafar@genesis.global>
2025-04-14 09:09:40 +0100

    chore: scheduler docs added PTL-1893

---
+++ examples/ui/client-capabilities/interaction/scheduler.js
@@ -0,0 +1,23 @@
+import { CodeSection } from '../../documentationBase';
+import useIsBrowser from '@docusaurus/useIsBrowser';
+import { useState } from 'react';
+
+export default function CronSchedulerDemo({ children, color }) {
+
+  const isBrowser = useIsBrowser();
+
+	if (isBrowser) {
+		const RapidImports = require('../../rapidImports');
+		RapidImports.registerComponents();
+	}
+	const [cron, setCron] = useState('0 30 10 ? * 1,3');
+
+	return (
+		<CodeSection>
+			<div style={{ color: 'var(--neutral-foreground-rest)', width: '100%', height: '300px'}}>
+				<rapid-scheduler-cron-builder cron="0 30 10 ? * 1,3" onChange={(e) => setCron(e.target.value)} >
+				</rapid-scheduler-cron-builder>
+				<p style={{ color: 'var(--neutral-foreground-rest)', fontSize: 'var(--type-ramp-base-font-size)', padding: 'calc(var(--design-unit) * 4px)'}}>Cron: {cron}</p>
+			</div>
+		</CodeSection>)
+}
