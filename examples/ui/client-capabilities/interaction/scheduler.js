e66ceea9c6e89761caa0074812327039ee01e656
mahamzafar990 <maham.zafar@genesis.global>
2025-04-15 12:03:55 +0100

    chore: added card for scheduler in interaction-index PTL-1893

--- examples/ui/client-capabilities/interaction/scheduler.js
+++ examples/ui/client-capabilities/interaction/scheduler.js
@@ -2,7 +2,7 @@ import { CodeSection } from '../../documentationBase';
 import useIsBrowser from '@docusaurus/useIsBrowser';
 import { useState } from 'react';
 
-export default function CronSchedulerDemo({ children, color }) {
+export default function CronSchedulerDemo({ inIndex=false }) {
 
   const isBrowser = useIsBrowser();
 
@@ -11,10 +11,11 @@ export default function CronSchedulerDemo({ children, color }) {
 		RapidImports.registerComponents();
 	}
 	const [cron, setCron] = useState('0 30 10 ? * 2,4');
+	const styles = inIndex ? { color: 'var(--neutral-foreground-rest)', width: '100%', height: '200px', overflow: 'hidden'} : { color: 'var(--neutral-foreground-rest)', width: '100%', height: '350px'}
 
 	return (
 		<CodeSection>
-			<div style={{ color: 'var(--neutral-foreground-rest)', width: '100%', height: '350px'}}>
+			<div style={styles}>
 				<rapid-scheduler-cron-builder cron="0 30 10 ? * 2,4" onChange={(e) => setCron(e.target.value)} >
 				</rapid-scheduler-cron-builder>
 				<p style={{ color: 'var(--neutral-foreground-rest)', fontSize: 'var(--type-ramp-base-font-size)', padding: 'calc(var(--design-unit) * 4px)'}}>Cron: {cron}</p>
