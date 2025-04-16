3237cc32a49729aeab3c674af44e69941587d508
mahamzafar990 <maham.zafar@genesis.global>
2025-04-16 15:03:59 +0100

    chore: addressed Pr feedback PTL-1893

--- examples/ui/client-capabilities/interaction/scheduler.js
+++ examples/ui/client-capabilities/interaction/scheduler.js
@@ -1,6 +1,6 @@
 import { CodeSection } from '../../documentationBase';
 import useIsBrowser from '@docusaurus/useIsBrowser';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function CronSchedulerDemo({ inIndex=false }) {
 
@@ -10,13 +10,23 @@ export default function CronSchedulerDemo({ inIndex=false }) {
 		const RapidImports = require('../../rapidImports');
 		RapidImports.registerComponents();
 	}
+
+	const schedulerRef = useRef(null);
+
 	const [cron, setCron] = useState('0 30 10 ? * 2,4');
+
+	useEffect(() => {
+		if (schedulerRef.current) {
+		  schedulerRef.current.cron = cron;
+		}
+	  }, [cron]);
+
 	const styles = inIndex ? { color: 'var(--neutral-foreground-rest)', width: '100%', height: '200px', overflow: 'hidden'} : { color: 'var(--neutral-foreground-rest)', width: '100%', height: '350px'}
 
 	return (
 		<CodeSection>
 			<div style={styles}>
-				<rapid-scheduler-cron-builder cron="0 30 10 ? * 2,4" onChange={(e) => setCron(e.target.value)} >
+				<rapid-scheduler-cron-builder ref={schedulerRef}  cron={cron} onChange={(e) => setCron(e.target.value)} >
 				</rapid-scheduler-cron-builder>
 				<p style={{ color: 'var(--neutral-foreground-rest)', fontSize: 'var(--type-ramp-base-font-size)', padding: 'calc(var(--design-unit) * 4px)'}}>Cron: {cron}</p>
 			</div>
